83f8245e8f8c56d624fe4baf8c8c8574456e238c
ATik HaSan <112861523+atikhasan392@users.noreply.github.com>
2025-01-05 02:42:17 +0600

    install_extensions.sh

--- install_extensions.sh
+++ install_extensions.sh
@@ -13,16 +13,21 @@ code --install-extension pkief.material-icon-theme # Material Icon Theme
 code --install-extension monokai.theme-monokai-pro-vscode # Monokai Pro
 code --install-extension bmewburn.vscode-intelephense-client # PHP Intelephense
 code --install-extension onecentlin.laravel-blade # Laravel Blade Snippets
+code --install-extension absszero.vscode-laravel-goto # Laravel Goto
+code --install-extension ryannaddy.laravel-artisan # Laravel Artisan
+code --install-extension amiralizadeh9480.laravel-extra-intellisense # Laravel Extra Intellisense
+code --install-extension austenc.laravel-blade-spacer # Laravel Blade Spacer
 code --install-extension editorconfig.editorconfig # EditorConfig for VS Code
 code --install-extension mikestead.dotenv # DotENV
 code --install-extension shufo.vscode-blade-formatter # Laravel Blade formatter
+code --install-extension onecentlin.laravel5-snippets # Laravel Snippets
+code --install-extension mohamedbenhida.laravel-intellisense # laravel intellisense
 code --install-extension damms005.devdb # DevDb
 code --install-extension pgl.laravel-jump-controller # laravel-jump-controller
 code --install-extension naoray.laravel-goto-components # laravel-goto-components
 code --install-extension bracketpaircolordlw.bracket-pair-color-dlw # Bracket Pair Color DLW
 code --install-extension sibiraj-s.vscode-scss-formatter # SCSS Formatter
 code --install-extension glenn2223.live-sass # Live Sass Compiler
-code --install-extension kisstkondoros.vscode-gutter-preview # Image preview
 code --install-extension davidanson.vscode-markdownlint # Markdown All in One
 code --install-extension christian-kohler.path-intellisense # Path Intellisense
 code --install-extension shardulm94.trailing-spaces # Trailing Spaces
@@ -39,3 +44,4 @@ code --install-extension zobo.php-intellisense # PHP IntelliSense
 code --install-extension xabikos.javascriptsnippets # JavaScript (ES6) code snippets
 code --install-extension sidthesloth.svg-snippets # SVG Snippets
 code --install-extension simonsiefke.svg-preview # Svg Preview
+code --install-extension ms-vscode.vscode-speech # VS Code Speech
