bcfd9c089077852141a36a7fba9b594307b89fa5
ATik HaSan <atikhasan2700@gmail.com>
2025-12-18 22:10:07 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,13 @@ code --install-extension naumovs.color-highlight # Color Highlight
 code --install-extension formulahendry.auto-close-tag # Auto Close Tag
 code --install-extension formulahendry.auto-rename-tag # Auto Rename Tag
 code --install-extension aaron-bond.better-comments # Better Comments
-code --install-extension dracula-theme.theme-dracula # Dracula Theme Official
 code --install-extension dbaeumer.vscode-eslint # ESLint
 code --install-extension github.codespaces # GitHub Codespaces
 code --install-extension pkief.material-icon-theme # Material Icon Theme
 code --install-extension monokai.theme-monokai-pro-vscode # Monokai Pro
 code --install-extension bmewburn.vscode-intelephense-client # PHP Intelephense
-code --install-extension muath-ye.composer-intelephense # Composer Intelephense
 code --install-extension onecentlin.laravel-blade # Laravel Blade Snippets
 code --install-extension absszero.vscode-laravel-goto # Laravel Goto
-code --install-extension ryannaddy.laravel-artisan # Laravel Artisan
-code --install-extension amiralizadeh9480.laravel-extra-intellisense # Laravel Extra Intellisense
 code --install-extension austenc.laravel-blade-spacer # Laravel Blade Spacer
 code --install-extension mikestead.dotenv # DotENV
 code --install-extension onecentlin.laravel5-snippets # Laravel Snippets
@@ -23,44 +19,22 @@ code --install-extension mohamedbenhida.laravel-intellisense # laravel intellise
 code --install-extension pgl.laravel-jump-controller # laravel-jump-controller
 code --install-extension naoray.laravel-goto-components # laravel-goto-components
 code --install-extension bracketpaircolordlw.bracket-pair-color-dlw # Bracket Pair Color DLW
-code --install-extension sibiraj-s.vscode-scss-formatter # SCSS Formatter
-code --install-extension glenn2223.live-sass # Live Sass Compiler
-code --install-extension davidanson.vscode-markdownlint # Markdown All in One
 code --install-extension christian-kohler.path-intellisense # Path Intellisense
 code --install-extension shardulm94.trailing-spaces # Trailing Spaces
 code --install-extension katsute.code-background # Background
 code --install-extension github.copilot # GitHub Copilot
-code --install-extension kisstkondoros.vscode-gutter-preview # Image preview
 code --install-extension bradlc.vscode-tailwindcss # Tailwind CSS IntelliSense
 code --install-extension joy-yu.css-snippets # CSS Snippets
 code --install-extension zignd.html-css-class-completion # IntelliSense for CSS class names in HTML
 code --install-extension solnurkarim.html-to-css-autocompletion # HTML to CSS autocompletion
 code --install-extension anteprimorac.html-end-tag-labels # HTML End Tag Labels
 code --install-extension xdebug.php-debug # PHP Debug
-code --install-extension zobo.php-intellisense # PHP IntelliSense
-code --install-extension xabikos.javascriptsnippets # JavaScript (ES6) code snippets
 code --install-extension simonsiefke.svg-preview # Svg Preview
-code --install-extension tabbyml.vscode-tabby # Tabby
 code --install-extension astro-build.astro-vscode # Astro
-code --install-extension adrianwilczynski.alpine-js-intellisense # Alpine.js IntelliSense
+code --install-extension pcbowers.alpine-intellisense # Alpine.js IntelliSense
 code --install-extension github.vscode-github-actions # GitHub Actions
-code --install-extension shufo.vscode-blade-formatter # Laravel Blade formatter
 code --install-extension ms-vscode-remote.remote-wsl # WSL
-code --install-extension ms-azuretools.vscode-docker # Docker
-code --install-extension ms-azuretools.vscode-containers # Container Tools
-code --install-extension docker.docker # Docker DX
 code --install-extension adrianwilczynski.alpine-js-intellisense # Alpine.js IntelliSense
-code --install-extension cierra.livewire-vscode # Livewire Language Support
-code --install-extension lennardv.livewire-goto-updated # Livewire goto
-code --install-extension oonfrs.livewire-support # Livewire Support
 code --install-extension laravel.vscode-laravel # Laravel
-code --install-extension codingyu.laravel-goto-view # Laravel goto view
 code --install-extension devsense.composer-php-vscode # Composer
-code --install-extension william-voyek.vscode-nginx # NGINX Configuration
-code --install-extension ahmadalli.vscode-nginx-conf # NGINX Configuration Language Support
-code --install-extension mrmlnc.vscode-apache # Apache Conf
-code --install-extension eiminsasete.apacheconf-snippets # Apache Conf Snippets
-code --install-extension redis.redis-for-vscode #Redis for VS Code
-code --install-extension cweijan.vscode-redis-client # Redis
-code --install-extension vunguyentuan.vscode-postcss # PostCSS Intellisense and Highlighting
-code --install-extension humao.rest-client # REST Client
+code --install-extension tabbyml.vscode-tabby # Tabby
