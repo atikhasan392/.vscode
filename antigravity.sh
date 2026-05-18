a52740f080e202cece6cd50a52fbb54edad2eab2
ATik HaSan <atikhasan2700@gmail.com>
2026-05-18 16:00:05 +0600

    Create antigravity.sh

---
+++ antigravity.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+antigravity --install-extension ritwickdey.LiveServer # Live Server
+antigravity --install-extension vincaslt.highlight-matching-tag # Highlight Matching Tag
+antigravity --install-extension naumovs.color-highlight # Color Highlight
+antigravity --install-extension formulahendry.auto-close-tag # Auto Close Tag
+antigravity --install-extension formulahendry.auto-rename-tag # Auto Rename Tag
+antigravity --install-extension aaron-bond.better-comments # Better Comments
+antigravity --install-extension dbaeumer.vscode-eslint # ESLint
+antigravity --install-extension pkief.material-icon-theme # Material Icon Theme
+antigravity --install-extension monokai.theme-monokai-pro-vscode # Monokai Pro
+antigravity --install-extension onecentlin.laravel-blade # Laravel Blade Snippets
+antigravity --install-extension absszero.vscode-laravel-goto # Laravel Goto
+antigravity --install-extension austenc.laravel-blade-spacer # Laravel Blade Spacer
+antigravity --install-extension mikestead.dotenv # DotENV
+antigravity --install-extension onecentlin.laravel5-snippets # Laravel Snippets
+antigravity --install-extension mohamedbenhida.laravel-intellisense # laravel intellisense
+antigravity --install-extension davidanson.vscode-markdownlint # markdownlint
+antigravity --install-extension naoray.laravel-goto-components # laravel-goto-components
+antigravity --install-extension bracketpaircolordlw.bracket-pair-color-dlw # Bracket Pair Color DLW
+antigravity --install-extension christian-kohler.path-intellisense # Path Intellisense
+antigravity --install-extension shardulm94.trailing-spaces # Trailing Spaces
+antigravity --install-extension katsute.code-background # Background
+antigravity --install-extension github.copilot # GitHub Copilot
+antigravity --install-extension bradlc.vscode-tailwindcss # Tailwind CSS IntelliSense
+antigravity --install-extension joy-yu.css-snippets # CSS Snippets
+antigravity --install-extension zignd.html-css-class-completion # IntelliSense for CSS class names in HTML
+antigravity --install-extension solnurkarim.html-to-css-autocompletion # HTML to CSS autocompletion
+antigravity --install-extension anteprimorac.html-end-tag-labels # HTML End Tag Labels
+antigravity --install-extension xdebug.php-debug # PHP Debug
+antigravity --install-extension simonsiefke.svg-preview # Svg Preview
+antigravity --install-extension astro-build.astro-vscode # Astro
+antigravity --install-extension pcbowers.alpine-intellisense # Alpine.js IntelliSense
+antigravity --install-extension ms-vscode-remote.remote-wsl # WSL
+antigravity --install-extension adrianwilczynski.alpine-js-intellisense # Alpine.js IntelliSense
+antigravity --install-extension laravel.vscode-laravel # Laravel
+antigravity --install-extension devsense.composer-php-vscode # Composer
+antigravity --install-extension muath-ye.composer-intelephense # Composer Intelephense
+antigravity --install-extension tamasfe.even-better-toml # Even Better TOML
+antigravity --install-extension cierra.livewire-vscode # Livewire Language Support
+antigravity --install-extension ganyicz.livewire-sfc-support # Livewire SFC / Volt support
+antigravity --install-extension lennardv.livewire-goto-updated # Livewire goto
+antigravity --install-extension amiralizadeh9480.laravel-extra-intellisense # Laravel Extra Intellisense
+antigravity --install-extension codingyu.laravel-goto-view # Laravel goto view
+antigravity --install-extension juniorpeixoto.vscode-filamentphp-helpers # Filament PHP Helpers
+antigravity --install-extension esbenp.prettier-vscode # Prettier - Code formatter
+antigravity --install-extension openai.chatgpt # Codex – OpenAI’s coding agent
+antigravity --install-extension continue.continue # Continue - open-source AI code agent
+antigravity --install-extension devsense.phptools-vscode # PHP
+antigravity --install-extension google.gemini-cli-vscode-ide-companion # Gemini CLI Companion
+antigravity --install-extension christian-kohler.npm-intellisense # npm Intellisense
+antigravity --install-extension oven.bun-vscode # Bun for Visual Studio Code
